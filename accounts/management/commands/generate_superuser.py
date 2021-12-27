a250c6fc43c96eb4cdc9ded86c47e4bb6e171b04
ShafikurRahman <shafikshaon@gmail.com>
2021-12-28 00:58:57 +0600

    initial commit

---
+++ accounts/management/commands/generate_superuser.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Generate superuser'
+
+    def handle(self, *args, **kwargs):
+        User.objects.create_superuser(username='educative', password='p@ss1234')
+        self.stdout.write('Create superuser.')
